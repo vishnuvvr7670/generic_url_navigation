c0c291100dac1c658b5f5d15f732e16f40c44c26
vishnuvvr7670 <vishnuvvr7670@gmil.com>
2023-11-22 11:20:49 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+# Create your views here.
+def vishnu(request):
+    return render(request,'vishnu.html')
+
+def srujana(request):
+    return render(request,'srujana.html')
+
+def vishnu2(request):
+    return render(request,'vishnu2.html')
